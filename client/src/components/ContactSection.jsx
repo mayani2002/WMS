d7390d3546e61708003719971e41670a8dd4f373
mayani <arti.agni01@gmail.com>
2022-09-26 18:17:19 +0530

    Created Contact us Section

---
+++ client/src/components/ContactSection.jsx
@@ -0,0 +1,45 @@
+import { Typography, Box, Button, TextField, Link } from '@mui/material';
+import React from 'react';
+
+const ContactSection = () => {
+    return (
+        <Box sx={{ ml: 17 ,mb:23, display: "flex" }}>
+            <Box >
+                <Typography variant="h3">Contact</Typography>
+                <Typography sx={{ mt: 3, mb: 3, width: "300px" }} variant="subtitle2">Questions or concerns?Just fill out the form below and our support team will get back to you within 24 hours </Typography>
+
+            </Box >
+            <Box
+                component="form"
+                sx={{ml:"30%",
+                    '& > :not(style)': { m: 2, width: "300px" },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <Box sx={{ display: "flex" }}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="First Name"
+                        defaultValue=""
+                    // onChange={handleChange}
+                    />
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Last Name"
+                        defaultValue=""
+                    // onChange={handleChange}
+                   />
+                </Box>
+                <TextField fullWidth label="fullWidth" id="fullWidth" />
+                <Box>
+                <Button sx={{ ml:10, backgroundColor: "black", alignItems:"center"}} variant="contained">Submit Now</Button>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default ContactSection;
