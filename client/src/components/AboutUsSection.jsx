10ff8f2dbcbf286d0f08379159a8ce60f159b481
mayani <arti.agni01@gmail.com>
2022-09-26 18:16:49 +0530

    Created About Us Section

---
+++ client/src/components/AboutUsSection.jsx
@@ -0,0 +1,18 @@
+import { Typography, Box, Button, Link } from '@mui/material';
+import React from 'react';
+
+const AboutUsSection = () => {
+    return (
+
+        <Box sx={{ m: 17 }}>
+            <Typography variant="h3">WHO ARE WE?</Typography>
+            <Typography sx={{ mt: 5, mb:3 , width:"600px"}} variant="subtitle2">Schedule your Grabage pickup 24 hours beforeSchedule your Grabage pickup 24 hours beforeSchedule your Grabage pickup 24 hours beforeSchedule your Grabage pickup 24 hours beforeSchedule your Grabage pickup 24 hours beforeSchedule your Grabage pickup 24 hours beforeSchedule your Grabage pickup 24 hours beforeSchedule your Grabage pickup 24 hours before </Typography>
+            <Box>
+                <Link>Learn more</Link>
+            </Box>
+        </Box >
+
+    )
+}
+
+export default AboutUsSection;
