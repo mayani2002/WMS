f5c19bdecc51fbb412f791c7ce17959539adb214
mayani <arti.agni01@gmail.com>
2022-09-27 13:03:09 +0530

    divided in components

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,15 @@
 import React, { Suspense, useContext } from 'react';
 
 import AccountProvider from './context/AccountProvider';
-import { AccountContext } from './context/AccountProvider';
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import UserInterface from './pages/UserInterface';
 import './App.css';
 
 
-// Pages
-const User = React.lazy(() => import('./pages/landing/User.jsx'))
-const Admin = React.lazy(() => import('./pages/admin/Admin.jsx'))
-const Citizen = React.lazy(() => import('./pages/citizen/Citizen.jsx'))
-const Driver = React.lazy(() => import('./pages/driver/Driver.jsx'))
-// const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-// const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
-
-
-
 function App() {
-  const { account } = useContext(AccountContext);
   
   return (
     <AccountProvider>
-    <HashRouter>
-      <Suspense >
-        <Routes>
-            <Route path="*" name="user" element={account ? <User /> : <User />} />
-          {/* <Route exact path="/citizen" name="Citizen Dashboard Page" element={} /> */}
-          <Route exact path="/admin" name="Admin Dashboard Page" element={<Admin />} />
-          <Route exact path="/driver" name="Driver Dashboard Page" element={<Driver />} />
-          {/* <Route exact path="/404" name="Page 404" element={<Page404 />} /> */}
-          {/* <Route exact path="/500" name="Page 500" element={<Page500 />} /> */}
-        </Routes>
-      </Suspense>
-      </HashRouter>
+      <UserInterface/>
     </AccountProvider>
   );
 }
---
+++ client/src/pages/UserInterface.jsx
@@ -0,0 +1,35 @@
+import React, { Suspense, useContext } from "react";
+import { AccountContext } from '../context/AccountProvider';
+import { HashRouter, Route, Routes } from 'react-router-dom'
+
+// Pages
+const User = React.lazy(() => import('./landing/User.jsx'))
+const Admin = React.lazy(() => import('./admin/Admin.jsx'))
+const Citizen = React.lazy(() => import('./citizen/Citizen.jsx'))
+const Driver = React.lazy(() => import('./driver/Driver.jsx'))
+// const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
+// const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+
+
+
+const UserInterface = () => {
+  const { account , setaccount} = useContext(AccountContext);
+    
+    return (
+        <HashRouter>
+            <Suspense >
+                <Routes>
+                    <Route path="*" name="user" element={account!=null ? <Citizen /> : <User />} />
+                    {/* <Route exact path="*" name="Citizen Dashboard Page" element={<Citizen />} /> */}
+                    <Route exact path="/admin" name="Admin Dashboard Page" element={<Admin />} />
+                    <Route exact path="/driver" name="Driver Dashboard Page" element={<Driver />} />
+                    {/* <Route exact path="/404" name="Page 404" element={<Page404 />} /> */}
+                    {/* <Route exact path="/500" name="Page 500" element={<Page500 />} /> */}
+                </Routes>
+            </Suspense>
+        </HashRouter>
+    )
+
+}
+
+export default UserInterface;
