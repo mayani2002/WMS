f28cc560158d41c1ea6f970feff8ca18087a0bd5
mayani <arti.agni01@gmail.com>
2022-09-27 11:44:45 +0530

    dependencie | account context provider

---
+++ client/src/context/AccountProvider.jsx
@@ -0,0 +1,30 @@
+
+// import { io } from 'socket.io-client';
+import React, { createContext, useEffect, useRef, useState } from "react";
+
+export const AccountContext = createContext(null);
+
+const AccountProvider = ({ children }) => {
+
+    const [account, setAccount] = useState();
+    const [newNotificationFlag, setNotificationFlag] = useState(false);
+
+    // const socket = useRef();
+
+    // useEffect(() => {
+    //     socket.current = io('ws://localhost:9000');
+    // }, [])
+
+    return (
+        <AccountContext.Provider value={{
+            account,
+            setAccount,
+            // socket,
+            newNotificationFlag,
+            setNotificationFlag
+        }}
+        >{children}
+        </AccountContext.Provider>
+    )
+}
+export default AccountProvider;
