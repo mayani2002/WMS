00c74818daa0a9054130f77bab8ee58ef729026f
Tanmay Singhania <tanmaysm1711@gmail.com>
2022-09-30 15:50:12 +0530

    Minor Bug Fixes

--- wms/src/components/Iconify.js
+++ wms/src/components/Iconify.js
@@ -7,10 +7,10 @@ import { Box } from '@mui/material';
 // ----------------------------------------------------------------------
 
 Iconify.propTypes = {
-  icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
-  sx: PropTypes.object,
+    icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+    sx: PropTypes.object,
 };
 
 export default function Iconify({ icon, sx, ...other }) {
-  return <Box component={Icon} icon={icon} sx={{ ...sx }} {...other} />;
+    return <Box component={Icon} icon={icon} sx={{ ...sx }} {...other} />;
 }
